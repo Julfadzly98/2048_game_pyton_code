6c1dfd2a05de904d068e064e62f41ce106832d48
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2021-05-28 09:32:29 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,248 +1,283 @@
-import tkinter as tk
-import random
-import colours as c
+import pygame,sys,time
+from pygame.locals import *
+from constants import *
+from random import *
 
 
-class Game(tk.Frame):
-    def __init__(self):
-        tk.Frame.__init__(self)
-        self.grid()
-        self.master.title('game')
-
-        self.main_grid = tk.Frame(
-            self, bg=c.GRID_COLOR, bd=3, width=400, height=400)
-        self.main_grid.grid(pady=(80, 0))
-        self.make_GUI()
-        self.start_game()
-
-        self.master.bind("<Left>", self.left)
-        self.master.bind("<Right>", self.right)
-        self.master.bind("<Up>", self.up)
-        self.master.bind("<Down>", self.down)
-
-        self.mainloop()
+BOARD_SIZE = 4
+TOTAL_POINTS = 0
+DEFAULT_SCORE = 2
 
 
-    def make_GUI(self):
-        # make grid
-        self.cells = []
-        for i in range(4):
-            row = []
-            for j in range(4):
-                cell_frame = tk.Frame(
-                    self.main_grid,
-                    bg=c.EMPTY_CELL_COLOR,
-                    width=100,
-                    height=100)
-                cell_frame.grid(row=i, column=j, padx=5, pady=5)
-                cell_number = tk.Label(self.main_grid, bg=c.EMPTY_CELL_COLOR)
-                cell_number.grid(row=i, column=j)
-                cell_data = {"frame": cell_frame, "number": cell_number}
-                row.append(cell_data)
-            self.cells.append(row)
-
-        # make score header
-        score_frame = tk.Frame(self)
-        score_frame.place(relx=0.5, y=40, anchor="center")
-        tk.Label(
-            score_frame,
-            text="Score",
-            font=c.SCORE_LABEL_FONT).grid(
-            row=0)
-        self.score_label = tk.Label(score_frame, text="0", font=c.SCORE_FONT)
-        self.score_label.grid(row=1)
 
 
-    def start_game(self):
-        # create matrix of zeroes
-        self.matrix = [[0] * 4 for _ in range(4)]
+pygame.init()
 
-        # fill 2 random cells with 2s
-        row = random.randint(0, 3)
-        col = random.randint(0, 3)
-        self.matrix[row][col] = 2
-        self.cells[row][col]["frame"].configure(bg=c.CELL_COLORS[2])
-        self.cells[row][col]["number"].configure(
-            bg=c.CELL_COLORS[2],
-            fg=c.CELL_NUMBER_COLORS[2],
-            font=c.CELL_NUMBER_FONTS[2],
-            text="2")
-        while(self.matrix[row][col] != 0):
-            row = random.randint(0, 3)
-            col = random.randint(0, 3)
-        self.matrix[row][col] = 2
-        self.cells[row][col]["frame"].configure(bg=c.CELL_COLORS[2])
-        self.cells[row][col]["number"].configure(
-            bg=c.CELL_COLORS[2],
-            fg=c.CELL_NUMBER_COLORS[2],
-            font=c.CELL_NUMBER_FONTS[2],
-            text="2")
+SURFACE = pygame.display.set_mode((400,500),0,32)
+pygame.display.set_caption("game")
 
-        self.score = 0
+myfont = pygame.font.SysFont("monospace",40)
+scorefont = pygame.font.SysFont("monospace",30)
+
+tileMatrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+undoMat = []
+
+def main(fromLoaded = False):
+    
+    if not fromLoaded:
+        placeRandomTile()
+        placeRandomTile()
+    printMatrix()
 
 
-    # Matrix Manipulation Functions
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            
+            if checkIfCanGo() == True:
+                if event.type == KEYDOWN:
+                    if isArrow(event.key):
+                        rotations = getRotations(event.key)
+                        addToUndo()
+                        for i in range(0,rotations):
+                            rotateMatrixClockwise()
 
-    def stack(self):
-        new_matrix = [[0] * 4 for _ in range(4)]
-        for i in range(4):
-            fill_position = 0
-            for j in range(4):
-                if self.matrix[i][j] != 0:
-                    new_matrix[i][fill_position] = self.matrix[i][j]
-                    fill_position += 1
-        self.matrix = new_matrix
+                        if canMove():
+                            moveTiles()
+                            mergeTiles()
+                            placeRandomTile()
 
+                        for j in range(0,(4-rotations)%4):
+                            rotateMatrixClockwise()
+                            
+                        printMatrix()
+            else: # just checking wait
+                printGameOver()
 
-    def combine(self):
-        for i in range(4):
-            for j in range(3):
-                if self.matrix[i][j] != 0 and self.matrix[i][j] == self.matrix[i][j + 1]:
-                    self.matrix[i][j] *= 2
-                    self.matrix[i][j + 1] = 0
-                    self.score += self.matrix[i][j]
+            if event.type == KEYDOWN:
+                global BOARD_SIZE
 
-
-    def reverse(self):
-        new_matrix = []
-        for i in range(4):
-            new_matrix.append([])
-            for j in range(4):
-                new_matrix[i].append(self.matrix[i][3 - j])
-        self.matrix = new_matrix
-
-
-    def transpose(self):
-        new_matrix = [[0] * 4 for _ in range(4)]
-        for i in range(4):
-            for j in range(4):
-                new_matrix[i][j] = self.matrix[j][i]
-        self.matrix = new_matrix
-
-
-    # Add a new 2 or 4 tile randomly to an empty cell
-
-    def add_new_tile(self):
-        row = random.randint(0, 3)
-        col = random.randint(0, 3)
-        while(self.matrix[row][col] != 0):
-            row = random.randint(0, 3)
-            col = random.randint(0, 3)
-        self.matrix[row][col] = random.choice([2, 4])
+                if event.key == pygame.K_r:
+                 
+                    reset()
+                if 50<event.key and 56 > event.key:
+                    
+                    BOARD_SIZE = event.key - 48
+                    reset()
+                if event.key == pygame.K_s:
+                   
+                    saveGameState()
+                elif event.key == pygame.K_l:
+                    loadGameState()
+                    
+                elif event.key == pygame.K_u:
+                    undo()
+                   
+        pygame.display.update()
 
 
-    # Update the GUI to match the matrix
 
-    def update_GUI(self):
-        for i in range(4):
-            for j in range(4):
-                cell_value = self.matrix[i][j]
-                if cell_value == 0:
-                    self.cells[i][j]["frame"].configure(bg=c.EMPTY_CELL_COLOR)
-                    self.cells[i][j]["number"].configure(
-                        bg=c.EMPTY_CELL_COLOR, text="")
-                else:
-                    self.cells[i][j]["frame"].configure(
-                        bg=c.CELL_COLORS[cell_value])
-                    self.cells[i][j]["number"].configure(
-                        bg=c.CELL_COLORS[cell_value],
-                        fg=c.CELL_NUMBER_COLORS[cell_value],
-                        font=c.CELL_NUMBER_FONTS[cell_value],
-                        text=str(cell_value))
-        self.score_label.configure(text=self.score)
-        self.update_idletasks()
+#  functions
+#  This module helps in check that it is possible to move or not  
 
-
-    # Arrow-Press Functions
-
-    def left(self, event):
-        self.stack()
-        self.combine()
-        self.stack()
-        self.add_new_tile()
-        self.update_GUI()
-        self.game_over()
+def canMove():
+    for i in range(0,BOARD_SIZE):
+        for j in range(1,BOARD_SIZE):
+            if tileMatrix[i][j-1] == 0 and tileMatrix[i][j] > 0:
+                return True 
+            elif (tileMatrix[i][j-1] == tileMatrix[i][j]) and tileMatrix[i][j-1] != 0:
+                return True
+    return False
+    # This module moves 
+def moveTiles():
+    for i in range(0,BOARD_SIZE):
+        for j in range(0,BOARD_SIZE-1):
+            
+            while tileMatrix[i][j] == 0 and sum(tileMatrix[i][j:]) > 0:
+                for k in range(j,BOARD_SIZE-1):
+                    tileMatrix[i][k] = tileMatrix[i][k+1]
+                tileMatrix[i][BOARD_SIZE-1] = 0
 
 
-    def right(self, event):
-        self.reverse()
-        self.stack()
-        self.combine()
-        self.stack()
-        self.reverse()
-        self.add_new_tile()
-        self.update_GUI()
-        self.game_over()
+# this is used for merging
+
+def mergeTiles():
+    global TOTAL_POINTS
+
+    for i in range(0,BOARD_SIZE):
+        for k in range(0,BOARD_SIZE-1):
+            if tileMatrix[i][k] == tileMatrix[i][k+1] and tileMatrix[i][k] != 0:
+                tileMatrix[i][k] = tileMatrix[i][k]*2
+                tileMatrix[i][k+1] = 0 # this was not intailized so the k value was going out the range value so by this we ever we merge the files it assigns the present value to zero and merge the number with the ahead value 
+                TOTAL_POINTS += tileMatrix[i][k]
+                moveTiles()
 
 
-    def up(self, event):
-        self.transpose()
-        self.stack()
-        self.combine()
-        self.stack()
-        self.transpose()
-        self.add_new_tile()
-        self.update_GUI()
-        self.game_over()
+#  this module helps in getting an random tile 
+def placeRandomTile():
+    c = 0
+    for i in range(0,BOARD_SIZE):
+        for j in range(0,BOARD_SIZE):
+            if tileMatrix[i][j] == 0:
+                c += 1
+    
+    k = floor(random() * BOARD_SIZE * BOARD_SIZE)
+    print("click")
+
+    while tileMatrix[floor(k/BOARD_SIZE)][k%BOARD_SIZE] != 0:
+        k = floor(random() * BOARD_SIZE * BOARD_SIZE)
+
+    tileMatrix[floor(k/BOARD_SIZE)][k%BOARD_SIZE] = 2
 
 
-    def down(self, event):
-        self.transpose()
-        self.reverse()
-        self.stack()
-        self.combine()
-        self.stack()
-        self.reverse()
-        self.transpose()
-        self.add_new_tile()
-        self.update_GUI()
-        self.game_over()
-        
-        
-    # Check if any moves are possible
 
-    def horizontal_move_exists(self):
-        for i in range(4):
-            for j in range(3):
-                if self.matrix[i][j] == self.matrix[i][j + 1]:
-                    return True
-        return False        
-        
+#  this is used to get floor value out of the given value to the module
+def floor(n):
+    return int(n - (n % 1 ))  
+# This module is used to print the given matrix
+def printMatrix():
+        SURFACE.fill(BLACK)
+        global BOARD_SIZE
+        global TOTAL_POINTS
 
-    def vertical_move_exists(self):
-        for i in range(3):
-            for j in range(4):
-                if self.matrix[i][j] == self.matrix[i + 1][j]:
-                    return True
-        return False
+        for i in range(0,BOARD_SIZE):
+            for j in range(0,BOARD_SIZE):
+                pygame.draw.rect(SURFACE,getColor(tileMatrix[i][j]),(i*(400/BOARD_SIZE),j*(400/BOARD_SIZE)+100,400/BOARD_SIZE,400/BOARD_SIZE))
+                label = myfont.render(str(tileMatrix[i][j]),1,(255,255,255))
+                label2 = scorefont.render("YourScore:"+str(TOTAL_POINTS),1,(255,255,255))
+                SURFACE.blit(label,(i*(400/BOARD_SIZE)+30,j*(400/BOARD_SIZE)+130))
+                SURFACE.blit(label2,(10,20))
 
 
-    # Check if Game is Over (Win/Lose)
+# We can call this an checker module
 
-    def game_over(self):
-        if any(2048 in row for row in self.matrix):
-            game_over_frame = tk.Frame(self.main_grid, borderwidth=2)
-            game_over_frame.place(relx=0.5, rely=0.5, anchor="center")
-            tk.Label(
-                game_over_frame,
-                text="You win!",
-                bg=c.WINNER_BG,
-                fg=c.GAME_OVER_FONT_COLOR,
-                font=c.GAME_OVER_FONT).pack()
-        elif not any(0 in row for row in self.matrix) and not self.horizontal_move_exists() and not self.vertical_move_exists():
-            game_over_frame = tk.Frame(self.main_grid, borderwidth=2)
-            game_over_frame.place(relx=0.5, rely=0.5, anchor="center")
-            tk.Label(
-                game_over_frame,
-                text="Game over!",
-                bg=c.LOSER_BG,
-                fg=c.GAME_OVER_FONT_COLOR,
-                font=c.GAME_OVER_FONT).pack()
+def checkIfCanGo():
+    for i in range(0,BOARD_SIZE ** 2): # This was having an error as that is 4 power of 2
+        if tileMatrix[floor(i/BOARD_SIZE)][i%BOARD_SIZE] == 0:
+            return True
+    
+    for i in range(0,BOARD_SIZE):
+        for j in range(0,BOARD_SIZE-1):
+            if tileMatrix[i][j] == tileMatrix[i][j+1]:
+                return True
+            elif tileMatrix[j][i] == tileMatrix[j+1][i]:
+                return True
+    return False
 
+# This module return an matrix rather than we can call it an list 
+def convertToLinearMatrix():
 
-def main():
-    Game()
+    mat = []
+    for i in range(0,BOARD_SIZE ** 2):
+        mat.append(tileMatrix[floor(i/BOARD_SIZE)][i%BOARD_SIZE])
 
+    mat.append(TOTAL_POINTS)
+    return mat
 
-if __name__ == "__main__":
+#  this module is the main reason to make the covert linearn function 
+def addToUndo():
+    undoMat.append(convertToLinearMatrix())   
+#  This module is used to mix up the matrix after a button/ move is done by the user
+def rotateMatrixClockwise():
+    for i in range(0,int(BOARD_SIZE/2)):
+        for k in range(i,BOARD_SIZE- i- 1):
+            temp1 = tileMatrix[i][k]
+            temp2 = tileMatrix[BOARD_SIZE - 1 - k][i]
+            temp3 = tileMatrix[BOARD_SIZE - 1 - i][BOARD_SIZE - 1 - k]
+            temp4 = tileMatrix[k][BOARD_SIZE - 1 - i]
+
+            tileMatrix[BOARD_SIZE - 1 - k][i] = temp1
+            tileMatrix[BOARD_SIZE - 1 - i][BOARD_SIZE - 1 - k] = temp2
+            tileMatrix[k][BOARD_SIZE - 1 - i] = temp3
+            tileMatrix[i][k] = temp4
+
+# when you dont have any place in the matrix or you are out of move then this module is called so it is in the else part of the main func
+def printGameOver():
+    global TOTAL_POINTS
+
+    SURFACE.fill(BLACK)
+
+    label = scorefont.render("GameOver!",1,(255,255,255))
+    label2 = scorefont.render("Score : "+str(TOTAL_POINTS),1,(255,255,255))
+    label3 = myfont.render("press 'R' to play again!! ",1,(255,255,255))
+
+    SURFACE.blit(label,(50,100))
+    SURFACE.blit(label2,(50,200))
+    SURFACE.blit(label3,(50,300))
+
+# This module is tot reset
+
+def reset():
+    global TOTAL_POINTS
+    global tileMatrix
+
+    TOTAL_POINTS = 0
+    SURFACE.fill(BLACK)
+    tileMatrix = [[0 for i in range(0,BOARD_SIZE)] for j in range(0,BOARD_SIZE) ]
     main()
+
+#  this saves the state of moves
+def saveGameState():
+    f = open("savedata","w")
+
+    line1 = " ".join([str(tileMatrix[floor(x/BOARD_SIZE)][x%BOARD_SIZE]) for x in range(0,BOARD_SIZE ** 2)])
+    f.write(line1+"\n")
+    f.write(str(BOARD_SIZE)+"\n")
+    f.write(str(TOTAL_POINTS))
+    f.close
+
+# this module is used to get the last move
+def undo():
+    if len(undoMat) > 0:
+        mat = undoMat.pop()
+
+        for i in range(0,BOARD_SIZE ** 2):
+            tileMatrix[floor(i/BOARD_SIZE)][i%BOARD_SIZE] = mat[i]
+        global TOTAL_POINTS 
+        TOTAL_POINTS  = mat[BOARD_SIZE ** 2]
+
+        printMatrix()
+
+def loadGameState():
+    global TOTAL_POINTS
+    global BOARD_SIZE
+    global tileMatrix
+
+    f = open("savedata","r")
+
+    mat = (f.readline()).split(' ',BOARD_SIZE ** 2)
+    BOARD_SIZE = int(f.readline())
+    TOTAL_POINTS = int(f.readline())
+
+    for i in range(0,BOARD_SIZE ** 2):
+        tileMatrix[floor(i/BOARD_SIZE)][i%BOARD_SIZE] = int(mat[i])
+
+    f.close()
+
+    main(True)
+
+
+# This help you find the which arrow is clicked by the user
+def isArrow(k):
+    return (k == pygame.K_UP or k == pygame.K_DOWN or k == pygame.K_LEFT or k == pygame.K_RIGHT)
+
+def getRotations(k):
+    if k == pygame.K_UP:
+        return 0
+    elif k == pygame.K_DOWN:
+        return 2 
+    elif k == pygame.K_LEFT:
+        return 1
+    elif k == pygame.K_RIGHT:
+        return 3
+
+# calling the main
+main()
+#  Ohh i need to get ready for errors .....
+
+
+''' 
+This is the video for 2048 game so you can find the source code or the program down in the description 
+Bye for now , we will meet again ......'''
